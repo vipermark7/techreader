5685e4a1401651983fc2f998188f2a7f6b1e1864
Shaffan Mustafa <shaffan1996@gmail.com>
2020-02-02 20:40:12 -0500

    Add files via upload
    Created a new Spring app using Java 11 and Web dependency with start.spring.io

---
+++ demo/src/main/java/com/techreads/TechreadsApplication.java
@@ -0,0 +1,13 @@
+package com.techreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechreadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechreadsApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/techreads/TechreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechreadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
